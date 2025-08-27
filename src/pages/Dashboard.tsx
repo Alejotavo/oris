c31bcbb6dfcb417619edc0602a33306073af7c59
Alejo <alejogustavof@gmail.com>
2025-08-27 11:06:56 -0300

    add ollama api service

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { getOllamaResponse } from '../services/ollamaApi'
+
+
+
+function Dashboard() {
+
+  const [userInput, setUserInput] = useState("")
+  const [responseData, setResponseData] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  
+
+  const handleSend = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await getOllamaResponse(userInput);
+      setResponseData(data.response);
+    } catch (err: any) {
+      setError(err.message || "Error al obtener la respuesta");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  } 
+  
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+     <input type="text" placeholder="Type your message here..." name='userInput' value={userInput} onChange={e => setUserInput(e.target.value)} />
+     <button onClick={handleSend}>Send</button>
+
+        <p>{responseData}</p>
+    </div>
+  );
+}
+export default Dashboard;
---
+++ src/services/ollamaApi.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:11434/api/generate";
+
+interface OllamaRequest {
+  model: string;
+  prompt: string;
+  stream: boolean;
+  response: string;
+}
+
+export const getOllamaResponse = async (inputText: string) => {
+  try {
+    const response = await axios.post<OllamaRequest>(`${API_BASE_URL}`, {
+      model: "gemma:2b",
+      prompt: inputText,
+      stream: false,
+      response: ""
+    });
+    console.log("Ollama API response:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching Ollama API response:", error);
+    throw error;
+  }
+};
